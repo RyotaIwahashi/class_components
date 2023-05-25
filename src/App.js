9636b7b40059d4405065a6223505134ed70a3f8f
RyotaIwahashi <r-iwahashi@groovenauts.jp>
2023-05-25 09:40:26 +0900

    handleClickの実装

--- src/App.js
+++ src/App.js
@@ -19,6 +19,15 @@ class App extends React.Component {
     })
   }
 
+  handleClick = () => {
+    const current = Math.floor(
+      Math.random() * this.state.anecdotes.length
+    )
+    this.setState({
+      current: current
+    })
+  } 
+
   render() {
     if (this.state.anecdotes.length === 0) {
       return <div>no anecdotes...</div>
@@ -30,9 +39,41 @@ class App extends React.Component {
         <div>
           {this.state.anecdotes[this.state.current].content}
         </div>
+        <button onClick={this.handleClick}>next</button>
       </div>
     )
   }
 }
 
 export default App
+
+
+
+// Functional component の場合は以下のようになる
+// Functional component を使用する注目すべき利点は、JavaScript クラスの自己参照 this を意識する必要がないこと
+// const App = () => {
+//   const [anecdotes, setAnecdotes] = useState([])
+//   const [current, setCurrent] = useState(0)
+
+//   useEffect(() =>{
+//     axios.get('http://localhost:3001/anecdotes').then(response => {
+//       setAnecdotes(response.data)
+//     })
+//   },[])
+
+//   const handleClick = () => {
+//     setCurrent(Math.round(Math.random() * (anecdotes.length - 1)))
+//   }
+
+//   if (anecdotes.length === 0) {
+//     return <div>no anecdotes...</div>
+//   }
+
+//   return (
+//     <div>
+//       <h1>anecdote of the day</h1>
+//       <div>{anecdotes[current].content}</div>
+//       <button onClick={handleClick}>next</button>
+//     </div>
+//   )
+// }
