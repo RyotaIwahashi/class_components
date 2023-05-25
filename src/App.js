55b7557522ab0160019ce75f62a5637e2a54718d
RyotaIwahashi <r-iwahashi@groovenauts.jp>
2023-05-25 09:32:52 +0900

    json-server からコンポーネントレンダリング時にデータを取得する

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React from 'react'
+import axios from 'axios'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      anecdotes: [],
+      current: 0,
+    }
+  }
+
+  componentDidMount = () => {
+    axios.get('http://localhost:3001/anecdotes').then(res => {
+      this.setState({
+        anecdotes: res.data
+      })
+    })
   }
 
   render() {
+    if (this.state.anecdotes.length === 0) {
+      return <div>no anecdotes...</div>
+    }
+
     return (
       <div>
         <h1>anecdote of the day</h1>
+        <div>
+          {this.state.anecdotes[this.state.current].content}
+        </div>
       </div>
     )
   }
